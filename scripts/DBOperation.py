637392d5e0c12d6a1cbd6503fe970604163dfa2f
zz <924797163@qq.com>
2018-12-14 22:41:24 +0800

    add opentsdb query tool

--- scripts/DBOperation.py
+++ scripts/DBOperation.py
@@ -31,10 +31,11 @@ class OpenTSDB:
         return r.json()['success'], r.json()['failed']
 
     def put(self, data, batch_size):
+        # use long session
         start_time = time.time()
         s = requests.Session()
         data_points = []
-        i = 1
+        count = 1
         success_num = 0
         failed_num = 0
         for item in data:
@@ -43,20 +44,56 @@ class OpenTSDB:
 
                 r = s.post(self.db_url + '/api/put?details',
                            json=data_points)
-                print('%d batch, %d success, %d failed' % (i, r.json()['success'], r.json()['failed']))
+                print('%d batch, %d success, %d failed' % (count, r.json()['success'], r.json()['failed']))
                 success_num = success_num + r.json()['success']
                 failed_num = failed_num + r.json()['failed']
                 data_points = []
-                i = i + 1
+                count = count + 1
         if len(data_points) > 0:
             r = s.post(self.db_url + '/api/put?details',
                        json=data_points)
-            print('%d batch, %d success, %d failed' % (i, r.json()['success'], r.json()['failed']))
+            print('%d batch, %d success, %d failed' % (count, r.json()['success'], r.json()['failed']))
             success_num = success_num + r.json()['success']
             failed_num = failed_num + r.json()['failed']
         print('Finished: %d success, %d failed.' % (success_num, failed_num))
         print(time.time() - start_time)
         return success_num, failed_num
+
+    def query(self, start, end, queries):
+        '''
+        :param start: timestamp
+        :param end: timestamp
+        :param queries: [Query]
+        :return:
+        '''
+        r = requests.post(self.db_url + '/api/put?details',
+                          json={
+                              'start': start,
+                              'end': end,
+                              'queries': [query.query_json for query in queries]
+                          })
+        return r.json()
+
+
+class Query:
+    def __init__(self, metric, tags={}, agg=None, filters=None, downsample=None):
+        '''
+        :param metric:
+        :param tags:
+        :param agg: aggregator eg. 'sum'
+        :param filters: [{'type', 'tagk', 'filter'}]
+        :param downsample: 't-fun' rg.'1m-sum'
+        '''
+        self.query_json = {
+            'metric': metric,
+            'tags': tags
+        }
+        if agg is not None:
+            self.query_json['aggregator'] = agg
+        if filters is not None:
+            self.query_json['filters'] = filters
+        if downsample is not None:
+            self.query_json['downsample'] = downsample
 
 
 if __name__ == '__main__':
@@ -70,10 +107,10 @@ if __name__ == '__main__':
                    'a': 0,
                    'b': 0
                })
-    data = []
+    d = []
     t = int(time.time()) - 100000
     for i in range(1000):
-        data.append({
+        d.append({
             'metric': 'taxi.test.balabala',
             'value': i,
             'timestamp': t + i,
@@ -82,4 +119,4 @@ if __name__ == '__main__':
                 'b': i
             }
         })
-    db.put(data=data, batch_size=60)
+    db.put(data=d, batch_size=60)
